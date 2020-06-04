5bc7f07e565edd35b104d8c37a0ddf30372c0a06
Viola Bai <44730845+viobai@users.noreply.github.com>
2020-06-04 08:55:17 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+var express    = require("express"),
+    app        = express(),
+    bodyParser = require("body-parser"),
+	ScubaSpot  = require("./models/scubaSpot"),
+    mongoose   = require("mongoose"),
+	flash = require("connect-flash"),
+	Comment    = require("./models/comment"),
+	passport = require("passport"),
+	LocalStrategy = require("passport-local"),
+	methodOverride = require("method-override"),
+	User       = require("./models/user"),
+	seedDB     = require("./seeds");
+
+var commentRoutes = require("./routes/comments"),
+	scubagroundRoutes = require("./routes/scubagrounds"),
+	indexRoutes = require("./routes/index"),
+	ratingRoutes = require("./routes/ratings"),
+	guideRoutes = require("./routes/localguides");
+	
+
+//seedDB(); "mongodb+srv://ViolaBai:yelpdive@yelpscuba-vou7v.mongodb.net/test?retryWrites=true&w=majority"
+var url = process.env.DATABASEURL || "mongodb+srv://ViolaBai:yelpdive@yelpscuba-vou7v.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(url,{useNewUrlParser: true,useCreateIndex:true}).then(() => {
+	console.log("connected to db!");
+}).catch(err => {
+	console.log("error", err.message);
+});
+
+app.set("view engine","ejs");
+app.use(express.static(__dirname+"/public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.use(flash());
+
+//passport config
+app.use(require("express-session")({
+	secret:"You are the cutest",
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req,res,next){
+	res.locals.currentUser = req.user;
+	res.locals.error = req.flash("error");
+	res.locals.success = req.flash("success");
+	next();
+})
+
+app.use("/",indexRoutes);
+app.use("/divingsites",scubagroundRoutes);
+app.use("/divingsites/:id/comments",commentRoutes);
+app.use("/divingsites/:id/ratings", ratingRoutes);
+app.use("/divingsites/:id/localguides",guideRoutes);
+
+
+
+
+app.listen(process.env.PORT||3000 , function(){ // 
+	console.log("The YelpDiving server has started!");
+});
